48b1d2135dabf9bc54519049e407fc6569854f5a
Igor Melnikov <igormeln2003@gmail.com>
2023-03-24 14:43:42 +0300

    added almost working agent-receiver

--- src/main/java/org/hse/bse/Main.java
+++ src/main/java/org/hse/bse/Main.java
@@ -3,6 +3,6 @@ package org.hse.bse;
 public class Main {
   public static void main(String[] args) {
     MainController mainController = new MainController();
-    mainController.initAgents("org.hse.jade.sample.agents");
+    mainController.initAgents("org.hse.bse.configuration");
   }
 }
--- src/main/java/org/hse/bse/configuration/TestAgent.java
+++ src/main/java/org/hse/bse/configuration/TestAgent.java
@@ -1,9 +1,13 @@
 package org.hse.bse.configuration;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
 
 import java.lang.annotation.Annotation;
 
+@JadeAgent(number = 5)
 public class TestAgent extends Agent implements JadeAgent {
   private static int counter = 0;
   private int currentNumber;
@@ -12,6 +16,19 @@ public class TestAgent extends Agent implements JadeAgent {
   public TestAgent() {
     currentNumber = ++counter;
     health = 100;
+  }
+
+  @Override
+  protected void setup() {
+    addBehaviour(new OneShotBehaviour() {
+      @Override
+      public void action() {
+        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+        msg.setContent("test Message");
+        msg.addReceiver(new AID("TestAgent", AID.ISLOCALNAME));
+        send(msg);
+      }
+    });
   }
 
   public int getHealth() {
