e4c3c141ec20798735d3e568474bb044ec0db7cc
Alena Vasileva <86800056+advasileva@users.noreply.github.com>
2023-03-24 16:43:13 +0300

    feat: add data provider

---
+++ src/main/java/org/hse/bse/utils/DataProvider.java
@@ -0,0 +1,22 @@
+package org.hse.bse.utils;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+import java.nio.file.Path;
+
+public class DataProvider {
+    private static Path resources = Path.of("src", "main", "resources");
+    public static JsonObject read(Data data) {
+        JsonParser parser = new JsonParser();
+        try {
+            return (JsonObject) parser.parse(
+                new FileReader(resources.resolve(data.getFilename()).toString())
+            );
+        } catch (java.io.FileNotFoundException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+}
