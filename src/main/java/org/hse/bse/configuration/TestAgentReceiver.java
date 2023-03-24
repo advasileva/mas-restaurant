12a597ee7bb24f850e9af753ac43616bbc1842df
Alena Vasileva <86800056+advasileva@users.noreply.github.com>
2023-03-24 16:53:28 +0300

    style: format

--- src/main/java/org/hse/bse/configuration/TestAgentReceiver.java
+++ src/main/java/org/hse/bse/configuration/TestAgentReceiver.java
@@ -3,50 +3,44 @@ package org.hse.bse.configuration;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
-
 import javax.swing.*;
-import java.lang.annotation.Annotation;
 
 @JadeAgent(number = 5)
-public class TestAgentReceiver extends Agent implements JadeAgent {
-    private static int counter = 0;
-    private int currentNumber;
-    private int health;
+public class TestAgentReceiver extends Agent {
+  private static int counter = 0;
+  private int currentNumber;
+  private int health;
 
-    public TestAgentReceiver() {
-        currentNumber = ++counter;
-        health = 100;
-    }
+  public TestAgentReceiver() {
+    currentNumber = ++counter;
+    health = 100;
+  }
 
-    @Override
-    protected void setup() {
-        addBehaviour(new CyclicBehaviour() {
-            @Override
-            public void action() {
-                ACLMessage msg = receive();
-                if (msg != null) {
-                    JOptionPane.showMessageDialog(null, "Content: " + msg.getContent());
-                } else {
-                    block();
-                }
+  @Override
+  protected void setup() {
+    addBehaviour(
+        new CyclicBehaviour() {
+          @Override
+          public void action() {
+            ACLMessage msg = receive();
+            if (msg != null) {
+              JOptionPane.showMessageDialog(null, "Content: " + msg.getContent());
+            } else {
+              block();
             }
+          }
         });
-    }
+  }
 
-    public int getHealth() {
-        return health;
-    }
+  public int getHealth() {
+    return health;
+  }
 
-    public String value() {
-        return "lol";
-    }
+  public String value() {
+    return "lol";
+  }
 
-    public int number() {
-        return currentNumber;
-    }
-
-    @Override
-    public Class<? extends Annotation> annotationType() {
-        return null;
-    }
+  public int number() {
+    return currentNumber;
+  }
 }
