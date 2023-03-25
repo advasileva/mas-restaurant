54df9b686309478522e9dcabfd5b11916e164633
advasileva <alenadvasileva@yandex.ru>
2023-03-25 10:52:00 +0300

    feat: backup

--- src/main/java/org/hse/bse/agents/VisitorAgent.java
+++ src/main/java/org/hse/bse/agents/VisitorAgent.java
@@ -4,6 +4,8 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 import org.hse.bse.configuration.JadeAgent;
@@ -12,6 +14,17 @@ import org.hse.bse.configuration.JadeAgent;
 public class VisitorAgent extends Agent {
     @Override
     protected void setup() {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("test-squad");
+        sd.setName("JADE-test");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
         System.out.println("init visitor.");
         addBehaviour(
             new OneShotBehaviour() {
